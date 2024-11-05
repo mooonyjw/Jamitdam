202b39dc9cceda7f2d88126f869cb877076ecb87
LeeHyo-Jeong <hyojung040102@naver.com>
2024-11-05 20:43:23 +0900

    Feat: View 디렉토리 추가

---
+++ Jamitdam/Jamitdam/View/CreateJamView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct CreateJamView: View {
+    // 하단의 다음 버튼을 활성화시키기 위한 boolean
+    // 인연이 선택되면 true가 된다.
+    @State private var isFocused: Bool = false
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                
+            }
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateJamView()
+    }
+}
