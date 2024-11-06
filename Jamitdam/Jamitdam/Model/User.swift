88a84b9337f74412a18b63b05c4aa4d205532c17
sonjueun <sonje515@hanyang.ac.kr>
2024-11-06 11:58:57 +0900

    Feat: User Image 추가

---
+++ Jamitdam/Jamitdam/Model/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  Jamitdam
+//
+//  Created by Jueun Son on 11/5/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct User{
+    var name: String     // 닉네임
+    var profile: String
+    var userID: String   // 사용자 입력 아이디
+    var password: String
+    var email: String
+    
+}
+var user1 = User(name: "유수현", profile: "UserProfile1", userID: "suhyeonU", password: "suhyeonU11", email: "Shu11@hanyang.ac.kr")
+var user2 = User(name: "진서기", profile: "UserProfile2", userID: "jinseoki", password: "jinseoki22", email: "Ljs22@hanyang.ac.kr")
+var user3 = User(name: "이모리", profile: "UserProfile3", userID: "limory", password: "limory33", email: "Lmr33@hanyang.ac.kr")
+var user4 = User(name: "메모리", profile: "UserProfile4", userID: "memory", password: "memory44", email: "Mmr44@hanyang.ac.kr")
+var user5 = User(name: "이혜민", profile: "UserProfile5", userID: "hyemin", password: "hyemin55", email: "Lhm55@hanyang.ac.kr")
+
