0de37cd17fb96539257a6e11846d148dfc645e02
sojeongyy <cindy0325@hanyang.ac.kr>
2024-11-06 11:15:30 +0900

    Feat: 구조체 수정

--- Jamitdam/Jamitdam/Model/Posts.swift
+++ Jamitdam/Jamitdam/Model/Posts.swift
@@ -9,18 +9,19 @@ import SwiftUI
 import Foundation
 
 struct Post {
-    let id: Int                 // 글 id
-    let comments: [String]      // 댓글 (댓글을 문자열 배열로 처리)
+    let id: UUID = UUID()       // 글 id
+    let comments: [Int]         // 댓글 (댓글을 문자열 배열로 처리)
     let body: String            // 본문
     let timestamp: Date         // 시각
-    let author: String          // 작성자
+    let author: Int             // 작성자
     let title: String           // 제목
     let likesCount: Int         // 좋아요 수
+    let hashTags: [String]      // 해시태그 배열
+    let relationshipIDs: [Int]  // 등장하는 인연 id
 }
 
 // 예시: 더미 데이터 생성
 let dummyPosts: [Post] = [
-    Post(id: 1, comments: ["첫 댓글!", "좋은 글 감사합니다."], body: "이것은 더미 데이터의 본문입니다.", timestamp: Date(), author: "작성자A", title: "더미 데이터 제목 1", likesCount: 5),
-    Post(id: 2, comments: ["흥미로운 내용이네요."], body: "여기도 더미 데이터 본문이 들어갑니다.", timestamp: Date(), author: "작성자B", title: "더미 데이터 제목 2", likesCount: 8),
-    Post(id: 3, comments: [], body: "댓글이 없는 게시글 더미 데이터.", timestamp: Date(), author: "작성자C", title: "더미 데이터 제목 3", likesCount: 2)
+    Post(comments: [1,2], body: "밥 뭐먹지", timestamp: Date(), author: 1, title: "점메추", likesCount: 5, hashTags: ["#친구"], relationshipIDs: [1]),
+    Post(comments: [3], body: "밥 먹자고 안하네", timestamp: Date(), author: 2, title: "답답해", likesCount: 8, hashTags: ["#썸남", "#답답"], relationshipIDs: [2]),
 ]
