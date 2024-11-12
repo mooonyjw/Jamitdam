af76a77418b5866d3786c36d719e48bb01646fd9
sojeongyy <cindy0325@hanyang.ac.kr>
2024-11-12 13:50:43 +0900

    Feat: user 구조체 요청온 친구 변수 추가

--- Jamitdam/Jamitdam/Model/User.swift
+++ Jamitdam/Jamitdam/Model/User.swift
@@ -16,7 +16,8 @@ struct User: Identifiable {
     var email: String
     var blockedFriends: [User] = []
     var friends: [User] = []
-    
+    var requestedFriends: [User] = []
+
     // 친구 추가
     mutating func addFriend(friend: User) {
         friends.append(friend)
@@ -40,7 +41,7 @@ struct User: Identifiable {
 
 
 
-var user1 = User(name: "유수현", profile: "UserProfile1", userID: "suhyeonU", password: "suhyeonU11", email: "Shu11@hanyang.ac.kr", blockedFriends: [user2, user3], friends: [user4, user5])
+var user1 = User(name: "유수현", profile: "UserProfile1", userID: "suhyeonU", password: "suhyeonU11", email: "Shu11@hanyang.ac.kr", blockedFriends: [user2, user3], friends: [user4, user5], requestedFriends: [user2, user5])
 
 var user2 = User(name: "진서기", profile: "UserProfile2", userID: "jinseoki", password: "jinseoki22", email: "Ljs22@hanyang.ac.kr")
 var user3 = User(name: "이모리", profile: "UserProfile3", userID: "limory", password: "limory33", email: "Lmr33@hanyang.ac.kr")
