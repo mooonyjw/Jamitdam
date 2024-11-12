dccb56be1c0a4724d11c4988b08ed39ff40c3104
mooonyjw <ljungwon91@gmail.com>
2024-11-12 11:21:03 +0900

    Feat: Login page 추가

---
+++ Jamitdam/Jamitdam/View/Login/Login.swift
@@ -0,0 +1,8 @@
+//
+//  Login.swift
+//  Jamitdam
+//
+//  Created by woni on 11/8/24.
+//
+
+import Foundation
--- Jamitdam/Jamitdam/View/Splash/SplashView.swift
+++ Jamitdam/Jamitdam/View/Splash/SplashView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SplashView: View {
     @State private var isAnimating = false
+    @State private var isTextVisible = false // Text visibility state
 
     var body: some View {
         VStack {
@@ -19,37 +20,60 @@ struct SplashView: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .blur(radius: 15)
-                    .frame(width: isAnimating ? 250 : 220, height: isAnimating ? 250 : 220)
+                    .frame(width: isAnimating ? 280 : 220, height: isAnimating ? 280 : 220)
                     .foregroundColor(Color("Redbase"))
                     .animation(
                         .easeInOut(duration: 0.5)
-                            .repeatCount(5),
+                            .repeatCount(3),
                         value: isAnimating
                     )
-                VStack{
+
+                VStack {
                     HStack {
                         Text("지금 가장\n잇한 이야기")
-                            .font(.system(size: 25, weight: <#T##Font.Weight?#>))
+                            .font(.title.bold())
+                            .offset(x: isTextVisible ? 0 : 0)
+                            .opacity(isTextVisible ? 1 : 0)
+                            .animation(.easeOut(duration: 0.5), value: isTextVisible)
                         Spacer()
                     }
                     .padding(.leading, 47)
                     .padding(.top, 180)
                     Spacer()
                 }
+                
+                VStack {
+                    HStack {
+                        Text("잼잇담")
+                            .font(.largeTitle.bold())
+                            .font(.system(size: 40, weight: .bold))
+                            .offset(x: isTextVisible ? 0 : 0)
+                            .opacity(isTextVisible ? 1 : 0)
+                            .animation(.easeOut(duration: 0.5), value: isTextVisible)
+                        Spacer()
+                    }
+                    .padding(.leading, 232)
+                    .padding(.top, 581)
+                    Spacer()
+                }
             }
-
+            Button("Button") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
             Spacer()
         }
         .background(Color("Whitebackground"))
         .ignoresSafeArea()
         .onAppear {
             isAnimating = true
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
+                isTextVisible = true
+            }
         }
     }
 }
 
-struct SplashView_Previews: PreviewProvider {
-    static var previews: some View {
-        SplashView()
-    }
+#Preview {
+    SplashView()
 }
