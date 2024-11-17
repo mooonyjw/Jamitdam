f488b14f838d40100da4e9dd00ae73e1b5a2a1b4
sonjueun <sonje515@hanyang.ac.kr>
2024-11-17 21:23:53 +0900

    Refactor: calendarView 코드 리팩토링에 따른 코드 분리 및 더미데이터 파일 수정

--- Jamitdam/Jamitdam/View/Calendar/CalendarView.swift
+++ Jamitdam/Jamitdam/View/Calendar/CalendarView.swift
@@ -1,165 +1,214 @@
-
 import Foundation
 import SwiftUI
 
 struct CalendarView: View {
-    @Binding var currentDate: Date
+    // 캘린더 가져오기
+
+    @State private var selectedDate: Date? = nil
+    // 상세보기 표시 여부
+    @State private var isDetailViewVisible = false
+    // 달력 메뉴
+    @State private var isMenuVisible = false
     
-    // Month update on arrow button clicks
-    @State var currentMonth: Int = 0
+    //@State private var selectedCalendar = "내 달력"
+    
+    @State var displayedCalendar: CalendarData = getCalendar(for: user1, from: dummyCalendars)
+    // 달력 유저
+    @State var selectedUser: User = user1
     
     var body: some View{
-        VStack(spacing: 8) {
-            // 내 달력
-            HStack(spacing: 20) {
-                Text("내 달력")
-                    .font(.title2.bold())
-                    //.frame(maxWidth: .infinity, alignment: .leading)
-                Button {
-                    withAnimation{
-                        
+        
+        ZStack(alignment: .topLeading) {
+            
+            if isDetailViewVisible {
+                // 배경을 탭하면 팝업 창 닫기
+                Color.whitebackground.opacity(0.01)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        isDetailViewVisible = false
                     }
-                } label: {
-                    Image(systemName: "chevron.down")
-                        .font(.callout)
-                        .foregroundColor(.black)
-                }
+            }
+            
+            
+            
+            VStack(spacing: 8) {
                 
+               
+                headerView()
+                monthSelector()
+                weekHeader()
+                dateGrid()
+                
+                // Posts
                 Spacer()
                 
-            }
-            .padding(.horizontal)
-            
-            // 연도, 월, 앞 뒤 버튼
-            HStack(spacing: 5) {
-                Text(extraDate()[0])
-                    .font(.headline)
-                    .fontWeight(.bold)
-                Text(extraDate()[1])
-                    .font(.headline)
-                    .fontWeight(.bold)
+                if isDetailViewVisible,
+                   let selectedDate = selectedDate, let postData = getPosts(for: selectedUser, from: dummyPosts).first(where: { displayedCalendar.isSameDay(date1: $0.timestamp, date2: selectedDate)}) {
+                               PostDetailView(postData: postData)
+                                   .padding()
+                                   .background(Color("Redemphasis").opacity(0.2))
+                                   .cornerRadius(30)
+                                   .frame(maxHeight: .none)
+                                   .transition(.move(edge: .bottom))
+                                   .animation(.easeInOut)
+                               
+                               }
                 
-                Spacer()
                 
-                HStack(spacing:35) {
-                    
-                    Button {
-                        withAnimation{
-                            currentMonth -= 1
-                        }
-                    } label: {
-                        Image(systemName: "chevron.left")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.redemphasis)
-                        
-                    }
-                    Button {
-                        withAnimation{
-                            currentMonth += 1
-                        }
-                        
-                    } label: {
-                        Image(systemName: "chevron.right")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.redemphasis)
-                        
-                    }
-                }    
             }
-            .padding(.horizontal)
+            .ignoresSafeArea(edges:.bottom)
             
-            // Days
-            let days: [String] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-            
-            
-            // Day View
-            HStack(spacing: 0) {
-                ForEach(days, id: \.self){ day in
-                    Text(day)
-                        .font(.callout)
-                        .foregroundColor(.graybasic)
-                        .frame(maxWidth: .infinity)
+            if isMenuVisible {
+                GeometryReader { geometry in
+                    CalendarMenuView( isMenuVisible: $isMenuVisible, displayedCalendar: $displayedCalendar, user: selectedUser, selectedUser: $selectedUser)
+                        .padding(.horizontal, 8)
+                        .padding(.top, 5)
+                        
+                       
                 }
-            }
-            
-            
-            // Dates
-            // Lazy Grid
-            let columns  = Array(repeating: GridItem(.flexible()), count: 7)
-            LazyVGrid(columns: columns, spacing: 15){
-                ForEach(extractDate()) { value in
-                   CardView(value: value)
-                        .background(
-                            Capsule()
-                                .fill(Color("Redemphasis"))
-                                .padding(.horizontal, 8)
-                                .opacity(isSameDay(date1: value.date, date2: currentDate) ? 1 : 0)
-                        
-                        )
-                        .onTapGesture {
-                            if hasTask(for: value.date) {
-                                currentDate = value.date
-                            }
-                            
-                        }
-                        .disabled((!hasTask(for: value.date)))
-                }
-            }
-            
-            // Tasks
-            
-            if let postData = dummyPosts.first(where: { isSameDay(date1: $0.timestamp, date2: currentDate)}) {
-                PostDetailView(postData: postData)
-                    .padding()
-                    .background(Color("Redemphasis").opacity(0.2))
-                    .cornerRadius(30)
-                    .padding(.top, 16)
-                    .frame(maxHeight: .none)
                 
-            }    
-        }
-        .ignoresSafeArea(edges:.bottom)
-        .onChange(of: currentMonth) { _ in
-            // updating Month
-            currentDate = getCurrentMonth()
-            
+            }
         }
     }
                                   
-          
-func hasTask(for date: Date)->Bool {
-    dummyPosts.contains{ isSameDay(date1: $0.timestamp, date2: date)}
-}
+    @ViewBuilder
+    func headerView() -> some View {
+        // 내 달력
+        HStack(spacing: 20) {
+            Text("\(selectedUser.name) 달력")
+                .font(.title2.bold())
+           
+            Button {
+                withAnimation{
+                    isMenuVisible.toggle()
+                }
+            } label: {
+                Image(systemName: "chevron.down")
+                    .font(.callout)
+                    .foregroundColor(.black)
+            }
+            
+            Spacer()
+            
+        }
+       
+        .padding()
+    }
+    
+    @ViewBuilder
+    func monthSelector() -> some View {
+        // 연도, 월, 앞 뒤 버튼
+        HStack(spacing: 5) {
+            Text(displayedCalendar.formattedDate(from: displayedCalendar.getCurrentMonth(), format: "yyyy년"))
+                .font(.headline)
+                .fontWeight(.bold)
+            Text(displayedCalendar.formattedDate(from: displayedCalendar.getCurrentMonth(), format: "MM월"))
+                .font(.headline)
+                .fontWeight(.bold)
+            
+            Spacer()
+            
+            HStack(spacing:35) {
+                
+                Button {
+                    withAnimation{
+                        displayedCalendar.currentMonthOffset -= 1
+                    }
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.redemphasis)
+                    
+                }
+                Button {
+                    withAnimation{
+                        displayedCalendar.currentMonthOffset += 1
+                    }
+                    
+                } label: {
+                    Image(systemName: "chevron.right")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.redemphasis)
+                    
+                }
+            }
+        }
+        
+        .padding(.horizontal)
+    }
+    
+    @ViewBuilder
+    func weekHeader()->some View {
+        // 요일
+        let days: [String] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+        
+        HStack(spacing: 0) {
+            ForEach(days, id: \.self){ day in
+                Text(day)
+                    .font(.callout)
+                    .foregroundColor(.graybasic)
+                    .frame(maxWidth: .infinity)
+            }
+        }
+    }
                                   
-                                  
+    @ViewBuilder
+    func dateGrid()->some View {
+        // 날짜
+        // Lazy Grid
+        let columns  = Array(repeating: GridItem(.flexible()), count: 7)
+        LazyVGrid(columns: columns, spacing: 15){
+            ForEach(displayedCalendar.extractDate()) { value in
+               // 날짜에 대한 개별 뷰
+                
+                CardView(value: value)
+                    .onTapGesture {
+                        
+                        if displayedCalendar.hasPost(for: value.date, from: getPosts(for: selectedUser, from: dummyPosts)){
+                            selectedDate = value.date
+                            isDetailViewVisible = true
+                        }
+                    }
+            }
+        }
+    }
+    
+    
+    // 날짜에 대한 개별 뷰
     @ViewBuilder
     func CardView(value: DateValue)->some View {
         VStack{
          
             if value.day != -1 {
-                if let post = dummyPosts.first(where: { post in
-                    
-                    return isSameDay(date1: post.timestamp, date2: value.date)
+                // 여러 개의 포스트가 있는 경우 추가하기
+                if let post = getPosts(for: selectedUser, from: dummyPosts).first(where: { post in
+                    displayedCalendar.isSameDay(date1: post.timestamp, date2: value.date)
                 }) {
                     Text("\(value.day)")
                         .font(.title3.bold())
-                        .foregroundColor(isSameDay(date1: post.timestamp, date2: currentDate) ? .white : .primary
+                        .foregroundColor(displayedCalendar.isSameDay(date1: value.date, date2: displayedCalendar.currentDate) ? .redemphasis : .primary
                         )
                         .frame(maxWidth: .infinity)
                     
                     Spacer()
+       
+                    ZStack{
+                        Circle()
+                            .fill(Color.redbase.opacity(0.7))
+                            .frame(width: 35, height: 35)
+                        Text(post.relationships[0].icon)
+                            .font(.title2)
+                    }
                     
-                    Circle()
-                        .fill(isSameDay(date1: post.timestamp, date2: currentDate) ? .white : Color("Redemphasis"))
-                        .frame(width: 8, height: 8)
+                   
                 }
                 else {
                     
                     Text("\(value.day)")
                         .font(.title3.bold())
-                        .foregroundColor(isSameDay(date1: value.date, date2: currentDate) ? .white : .primary)
+                        .foregroundColor(displayedCalendar.isSameDay(date1: value.date, date2: displayedCalendar.currentDate) ? .redemphasis : .primary)
                         .frame(maxWidth: .infinity)
                     
                     Spacer()
@@ -171,11 +220,14 @@ func hasTask(for date: Date)->Bool {
         .frame(height: 60, alignment: .top)
         
     }
+    
+    // post가 있는 날짜의 상세 뷰
     @ViewBuilder
     func PostDetailView(postData: Post) -> some View {
         VStack(alignment: .leading, spacing: 10) {
             HStack {
-                Text("\(extraDate()[0]) \(extraDate()[1]) \(Calendar.current.component(.day, from: currentDate))일")
+
+                Text(displayedCalendar.formattedDate(from: postData.timestamp, format: "yyyy년 MM월 dd일"))
                     .font(.title3.bold())
                 Spacer()
                 Button("삭제"){
@@ -202,89 +254,41 @@ func hasTask(for date: Date)->Bool {
                 .font(.subheadline)
                 .padding(.top, 8)
             
-            VStack(alignment: .leading, spacing: 20) {
-                
-                Text(postData.title ?? "")
-                    .font(.headline)
-           
-                Text(postData.content ?? "")
-           
+            VStack(alignment: .leading) {
+              
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(postData.title ?? "")
+                        .font(.headline)
+                    Text(postData.content ?? "")
+                    
+                }
             }
+            .frame(maxWidth: .infinity, alignment: .topLeading)
             .padding()
+            .padding(.bottom, 30)
             .background(Color("Whitebackground"))
             .cornerRadius(15)
-            
+       
         }
         .frame(maxHeight: .infinity, alignment: .top)
-        .ignoresSafeArea(edges: .bottom)
+        
         .padding(.top, 10)
+        .ignoresSafeArea(edges: .bottom)
         
     }
     
-    
-    // checking dates
-    func isSameDay(date1: Date, date2: Date) -> Bool {
-        let calendar = Calendar.current
-        return calendar.isDate(date1, inSameDayAs: date2)
-    }
-    
-    
-    // extrating Year and Month for display
-    func extraDate() -> [String] {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "YYYY년 MM월"
-        
-        let date = formatter.string(from: getCurrentMonth())
-        return date.components(separatedBy: " ")
-    }
-    
-    func getCurrentMonth() -> Date {
-        let calendar = Calendar.current
-        // Getting Current Month Date
-        guard let currentMonth = calendar.date(byAdding: .month, value: self.currentMonth, to: Date()) else{
-            return Date()
-        }
-        return currentMonth
-    }
-    
-    func extractDate() -> [DateValue] {
-        
-        let calendar = Calendar.current
-        // Getting Current Month Date
-        let currentMonth = getCurrentMonth()
-        var days = currentMonth.getAllDates().compactMap{date -> DateValue in
-            // getting day
-            let day = calendar.component(.day, from: date)
-            return DateValue(day: day, date: date)
-        }
-        
-        // adding offset days to get extra week day
-        let firstWeekday = calendar.component(.weekday, from: days.first?.date ?? Date())
-        for _ in 0..<firstWeekday - 1 {
-            days.insert(DateValue(day: -1, date: Date()), at: 0)
-        }
-        return days
-    }
-}
-    
-#Preview {
-    @State var currentDate = Date()
-    return CalendarView(currentDate: $currentDate)
+   
 }
 
-// Extending Date to get Current Month Dates
-extension Date {
-    func getAllDates() -> [Date] {
-        let calendar = Calendar.current
-        
-        // getting start Date
-        let startDate = calendar.date(from: Calendar.current.dateComponents([.year, .month], from: self))!
-        
-        let range = calendar.range(of: .day, in: .month, for: startDate)!
-        
-        // getting date
-        return range.compactMap { day -> Date in
-            return calendar.date(byAdding: .day, value: day-1, to: startDate)!
-        }
-    }
+
+    
+#Preview {
+   
+    let initialCalendar = getCalendar(for: user1, from: dummyCalendars)
+    var displayedCalendar = initialCalendar
+    // 기본적으로 본인 사용자로 초기화
+   var selectedUser = user1
+    
+    CalendarView(displayedCalendar: displayedCalendar, selectedUser: selectedUser)
+    
 }
