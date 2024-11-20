6690fe1d79f40c31feeda83caf27f2d407f16797
mooonyjw <ljungwon91@gmail.com>
2024-11-20 14:07:50 +0900

    Feat: 아이디 중복 처리

---
+++ Jamitdam/Jamitdam.xcodeproj/Jamitdam/Model/LoginID.swift
@@ -0,0 +1,18 @@
+//
+//  LoginID.swift
+//  Jamitdam
+//
+//  Created by woni on 11/15/24.
+//
+
+import SwiftUI
+
+struct LoginID: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginID()
+}
---
+++ Jamitdam/Jamitdam/View/Component/EmptyButton.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyButton.swift
+//  JamitDam
+//
+//  Created by woni on 11/18/24.
+//
+
+import SwiftUI
+
+struct EmptyButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyButton()
+}
