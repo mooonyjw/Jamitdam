3050ca17db83330297d79e82fa5b05935637be12
sonjueun <sonje515@hanyang.ac.kr>
2024-11-26 16:08:30 +0900

    Update: ContentView hello world로 수정

--- Jamitdam/Jamitdam/ContentView.swift
+++ Jamitdam/Jamitdam/ContentView.swift
@@ -7,23 +7,18 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-//    }
-//}
-struct ContentView: View{
-   
-    var body: some View{
-        EditView()
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
     }
 }
+
 #Preview {
     ContentView()
 }
