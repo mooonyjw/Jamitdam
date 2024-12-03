41c1294fc76fd6497a96c9706fa7d5537bc0f59f
mooonyjw <ljungwon91@gmail.com>
2024-12-03 12:28:26 +0900

    Feat: Home 추가

---
+++ Jamitdam/Jamitdam/View/Home/HomeView.swift
@@ -0,0 +1,93 @@
+import SwiftUI
+
+struct HomeView: View {
+    
+    // 글 주인공 (더미데이터 유수현)
+    private var writer: User = user1
+    // 글 (더미데이터 유수현의 글)
+    private var post: Post = dummyPosts[0]
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color("Whitebackground") // 배경색 설정
+                    .ignoresSafeArea() // 전체 화면에 배경색 적용
+                
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 56)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .padding(.top, 30)
+                
+                VStack {
+                    // 헤더 섹션
+                    HStack {
+                        
+                        // 검색 버튼
+                        Button(action: {
+                            // 검색 버튼 액션
+                        }) {
+                            Image(systemName: "magnifyingglass")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.horizontal, 6)
+                        // 알림 버튼
+                        Button(action: {
+                            // 알림 버튼 액션
+                        }) {
+                            Image(systemName: "bell")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                    .padding(.bottom, 40)
+                    
+                    Spacer() // 나머지 공간을 차지하게 하여 헤더가 위에 고정되도록 설정
+                    
+                    
+                    ScrollView{
+                        // "운명을 잇다" 섹션
+                        VStack(alignment: .leading, spacing: 8) {
+                            HStack(spacing: 6) {
+                                Text("재미를 잇다")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                                
+                                Image("HomeIcon")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                    .foregroundColor(.blue)
+                            }
+                            
+                            Text("한땀한땀 재미를 이어보아요.")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.horizontal)
+                        
+                        Spacer() // 나머지 공간 차지
+                        
+                    }
+                    .padding(.top)
+                }
+                .padding(.trailing, 8)
+                
+                
+            }
+        }
+    }
+}
+    
+#Preview {
+    HomeView()
+}
