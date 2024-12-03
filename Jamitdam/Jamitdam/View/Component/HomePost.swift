b487f9ba6b90228e67bc17d405e6eee0ddcf3c39
mooonyjw <ljungwon91@gmail.com>
2024-12-03 13:46:20 +0900

    Feat: HomePost 추가

---
+++ Jamitdam/Jamitdam/View/Component/HomePost.swift
@@ -0,0 +1,18 @@
+//
+//  HomePost.swift
+//  Jamitdam
+//
+//  Created by woni on 12/3/24.
+//
+
+import SwiftUI
+
+struct HomePost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePost()
+}
---
+++ Jamitdam/Jamitdam/View/PollPage/PollHomeVIew.swift
@@ -0,0 +1,18 @@
+//
+//  PollHomeVIew.swift
+//  Jamitdam
+//
+//  Created by woni on 12/3/24.
+//
+
+import SwiftUI
+
+struct PollHomeVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PollHomeVIew()
+}
